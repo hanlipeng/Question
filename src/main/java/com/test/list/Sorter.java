f714dd02bf190e473e6ebacc5b26acb4e259f7df
hanlipeng <593628242@qq.com>
2020-07-28 13:47:27 +0800

    排序算法

--- src/main/java/com/test/list/Sorter.java
+++ src/main/java/com/test/list/Sorter.java
@@ -7,7 +7,33 @@ package com.test.list;
 public class Sorter {
 
     public int[] sort(int[] a) {
+        sortPart(a, 0, a.length - 1);
         return a;
+    }
+
+    public void sortPart(int[] a, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = a[left];
+        int lIdx = left + 1;
+        int rIdx = right;
+        while (lIdx <= rIdx) {
+            while (a[lIdx] < mid && lIdx <= rIdx) lIdx++;
+            while (a[rIdx] > mid && lIdx <= rIdx) rIdx--;
+            if (lIdx < rIdx) {
+                switchNum(a, lIdx, rIdx);
+            }
+        }
+        switchNum(a, left, rIdx);
+        sortPart(a, left, rIdx-1);
+        sortPart(a, rIdx + 1, right);
+    }
+
+    private void switchNum(int[] a, int lIdx, int rIdx) {
+        int tmp = a[lIdx];
+        a[lIdx] = a[rIdx];
+        a[rIdx] = tmp;
     }
 
 }
